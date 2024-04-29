68b78de7bcff6ef920a94e7596cf1b50a721ecbd
Aryan Gupta <142935324+GitAryan45@users.noreply.github.com>
2024-04-29 16:42:31 +0000

    new file added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof "Aryan"); // typeof return string type
+console.log(typeof undefined); // typeof will return the type ie undefined
+console.log(typeof null); // returns typpe i.e object
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -3,6 +3,11 @@ const accountId =144553
 let accountEmail="aryan@google.com"
 var accountPassword="12345"
 accountCity="jaipur"
+let accountState
 
+accountEmail="guptaji@gmial.com"
+accountPassword="456456"
+accountCity="Bengaluru"
 
-console.log(accountId);
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountState])
