a2b56272075be96585b0b6ff31d8e324c5b1d35a
Aryan Gupta <142935324+GitAryan45@users.noreply.github.com>
2024-04-29 17:56:45 +0000

    modifiactions in converstionoperation wali file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,3 +32,44 @@ let somenumber = 33;
 let StringNumber = String(somenumber);//conversion to string datatype
 console.log(typeof StringNumber);
 console.log(StringNumber);
+
+// ***************** OPERATIONS *************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Aryan"
+
+let str3 = str1 + str2  //concatenation
+console.log(str3);
+
+// console.log("1" + 2);       //this will return 12
+// console.log(1 + "2");       // =>12 (isme bhi pehla element number h to pehle addition hoga fir string concatenation hoga ,,,,,, lekin pehle element k sath koi number nhi h to waisa hi rh jaega isi liye 12 print huA )
+// console.log("1" + 2 + 2);   // =>122
+// console.log(1 + 2 + "2");   // => 32 (kuki agr pehla element number h to addition wala concaenation hoga and last me string concatenate ho jaegi)
+
+console.log(+true); // + operator convert the anyother datatype to number datatype
+// kuki + operator ko increment operation perform krana hota h uske liye usko apne baad wale element ko number me convert krna padta h)
+// pr agr value/element k baad lgaige to error dega(console.log(true+))
+
+console.log(+" ");  //we know that empty string to number me convert krenge to 0 ayega 
+
+let x = 3n;
+let y = ++x; // prefix increment 
+console.log(x,y);
+
+let x2 = 3;
+const y2 = x2++; //postfix Increment
+console.log(x2,y2);// x is 4; y is 3
+
+// link to study conversion wala topic yourself
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
