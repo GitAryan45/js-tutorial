aa24274373d040984a26c6a1caee695f4f96d505
Aryan Gupta <142935324+GitAryan45@users.noreply.github.com>
2024-05-02 19:34:04 +0000

    learned funstioning of memory stack and heap

--- 01_basics/dataType_summary.js
+++ 01_basics/dataType_summary.js
@@ -40,3 +40,30 @@ console.log(typeof myFunction); // return function object
 
 //this is the link to study about datatypes 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//*************** MEMORY ******************************************
+//Stack(primitive) and heap(non-primitive (mtlb non primtive datatype k liye heap memory use hoti h ))
+
+// lets see how variables stores on stack memory
+let myYoutubename="AryanGuptadotcom"
+let anothername=myYoutubename //stores copy of myYoutubename not a refrence in case of stack memory
+console.log(anothername) // "AryanGuptadotcom" 
+
+anothername="mojodotcom"
+console.log(myYoutubename);//"AryanGuptadotcom" output
+console.log(anothername); //"mojodotcom"
+
+let userOne={                  // creating a object in js
+    upi:"user@ybl",
+    email: "user@google.com"
+    
+
+}
+//lets see how variables stores in heap memory
+let userTwo = userOne // (here value of userOne is not copied to user2 instead reference of userOne is assigned to usertwo)
+userTwo.email = "Aryan@google.com" // we are doing changes in usertwo wala object
+
+// now lets see ki kya changes ayege agr hm user2 me changes krte h
+console.log(userOne); // return upi='user@ybl' and email="Aryan@google.com"
+console.log(userTwo); // return upi='user@ybl' and email="Aryan@google.com"
+//mtlb ki agr second wale me chenge kroge to first m bhi change ho jaega kuki by referece assign hua tha
