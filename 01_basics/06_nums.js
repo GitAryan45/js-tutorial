12ef394c6de99725bdf1c494f35c2dafc7d17f65
Aryan Gupta <142935324+GitAryan45@users.noreply.github.com>
2024-05-05 21:06:29 +0000

    added some comments

--- 01_basics/06_nums.js
+++ 01_basics/06_nums.js
@@ -24,19 +24,20 @@ console.log(hundreds.toLocaleString('en-IN'));
 
 //++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++
 
-console.log(Math);
+console.log(Math); // output: Object [Maths] {}
 console.log(Math.abs(-4)); //absolute method used to convert negative number to positive Number, Output: 4
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
-console.log(Math.min(4, 3, 6, 8));
-console.log(Math.max(4, 3, 6, 8));
+console.log(Math.round(4.6)); // output: 5
+console.log(Math.ceil(4.2)); // output: 5
+console.log(Math.floor(4.9)); // output: 4
+console.log(Math.min(4, 3, 6, 8)); // output: 3
+console.log(Math.max(4, 3, 6, 8)); // output: 8
 
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
 
+// suppose we want random number between the given min and man range 
 const min = 10
 const max = 20
-
+// then this is a trick/ formula to print random Number between given Range
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
