f3fcd0ee153918da9b0f2cf0510b962c01b1b5a2
Aryan Gupta <142935324+GitAryan45@users.noreply.github.com>
2024-05-05 19:41:27 +0000

    added nums & maths file

---
+++ 01_basics/06_nums&maths.js
@@ -0,0 +1,8 @@
+const score = 400 // its a number type automatically by compiler
+console.log(score);
+
+const balance = Number(100) // here we are initialising number object 
+console.log(balance);
+// Number type properties/prototype trial
+console.log(balance.toString())
+console.log(balance.toString().length)
